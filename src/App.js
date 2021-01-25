1a21860e23cdc99f42dde1a3ab92ff13032ebf7d
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-01-25 09:47:38 +0300

    not working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+const styles = {
+  bold: { fontWeight: "bold" },
+  italic: { fontStyle: "italic" },
+  underline: { textDecorationLine: "underline" },
+};
+
+const stylings = ["bold", "italic", "underline"];
+
+const colors = ["yellow", "blue", "red", "black", "purple"];
+
+let userText = "";
 
 function App() {
+  const [text, setStyle] = useState();
+
+  const changeStyle = (style) => {
+    setStyle(userText.bold());
+    console.log(userText);
+    // if (fon) {
+    //   ReactDOM.render(element, document.getElementById("root"));
+    //   setStyle("dark");
+    // } else {
+    //   setStyle("light");
+    // }
+  };
+
+  const stylingBoxes = stylings.map((style) => (
+    <button
+      className="btn btn-light"
+      style={styles[style]}
+      key={style}
+      onClick={changeStyle(styles[style])}
+    >
+      {style}
+    </button>
+  ));
+
+  const colorBoxes = colors.map((color) => (
+    <button
+      style={{ backgroundColor: color, height: 30, width: 30 }}
+      key={color}
+    />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="my-3">{stylingBoxes}</div>
+      <textarea />
+      <div className="my-3">{colorBoxes}</div>
     </div>
   );
 }
